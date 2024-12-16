61f1442d6c7660d76c6a7e128b3f046a50818188
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-12-16 23:27:21 +0530

    Update dependancy.groovy

--- vars/dependancy.groovy
+++ vars/dependancy.groovy
@@ -1,4 +1,4 @@
 def call() {
        sh 'mvn org.owasp:dependency-check-maven:check -Dformat=HTML'
-       sh 'archiveArtifacts artifacts: 'target/dependency-check-report.html', allowEmptyArchive: true'
+       
     }
