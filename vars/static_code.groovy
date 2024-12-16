9412b72cb0767f22b829fa50e9648f8d69f23c55
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-12-16 23:05:01 +0530

    Update static_code.groovy

--- vars/static_code.groovy
+++ vars/static_code.groovy
@@ -1,11 +1,10 @@
 def call() {
-       withSonarQubeEnv('SonarQube') {
-                    sh ''' 
-                        sonar/bin/sonar-scanner \
-                        -Dsonar.projectKey=salary \
-                        -Dsonar.projectName=salary \
-                        -Dsonar.java.binaries=./target
-                    '''
-                }
-            }
+    withSonarQubeEnv('SonarQube') {
+        sh ''' 
+            sonar/bin/sonar-scanner \
+            -Dsonar.projectKey=salary \
+            -Dsonar.projectName=salary \
+            -Dsonar.java.binaries=./target
+        '''
     }
+}
