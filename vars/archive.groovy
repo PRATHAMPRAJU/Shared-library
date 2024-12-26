abb2a27264444a0cfed5e86486a03477f0e68b5c
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-12-26 14:01:22 +0530

    Update archive.groovy

--- vars/archive.groovy
+++ vars/archive.groovy
@@ -1,7 +1,7 @@
 def call() {
-    // Dependency
-    archiveArtifacts artifacts: 'target/dependency-check-report.html', allowEmptyArchive: true
+    // Dependency check report archiving
+    archiveArtifacts artifacts: 'target/dependency-check-report.html', allowEmptyArchive: true, includes: '**/*.html'
 
-    // DAST 
-    archiveArtifacts artifacts: 'nikto_report.html', allowEmptyArchive: true
+    // DAST report archiving
+    archiveArtifacts artifacts: 'nikto_report.html', allowEmptyArchive: true, includes: '**/*.html'
 }
